092b9a51777076ea948f866701a109ab0811fef0
tougshire <tougshire@tougshire.com>
2022-02-22 15:28:23 -0500

    add url redirects
    Import RecirectView  and reverse_lazy in urls.py, and add urls that redirect to other views

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.views.generic.base import RedirectView
+from django.urls import path, reverse_lazy
 from . import views
 
 app_name = 'wishlist'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('wishlist:itemrequest-list'))),
+    path('wishlist/', RedirectView.as_view(url=reverse_lazy('wishlist:itemrequest-list'))),
     path('itemrequest/create/', views.ItemRequestCreate.as_view(), name='itemrequest-create'),
     path('itemrequest/<int:pk>/update/', views.ItemRequestUpdate.as_view(), name='itemrequest-update'),
     path('itemrequest/<int:pk>/detail/', views.ItemRequestDetail.as_view(), name='itemrequest-detail'),
